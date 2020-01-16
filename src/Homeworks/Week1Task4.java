d5ee333084741fc8d08392e83cfb945c46940829
SalOnGit <1salnik1@gmail.com>
2020-01-16 18:24:37 +0200

    Task4

--- src/Homeworks/Week1Task4.java
+++ src/Homeworks/Week1Task4.java
@@ -1,24 +1,25 @@
 package Homeworks;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Week1Task4 {
 
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in).useLocale(Locale.US);
 
-        System.out.print("Enter the number please: ");
+        System.out.println("Enter the number please (as a decimal use a dot): ");
 
         double num = sc.nextDouble();
 
         if (num > 0 && num < 1) {
 
-            System.out.println("The number " + num + "is in range between 0 and 1");
+            System.out.println("The number " + num + " is in range between 0 and 1");
 
         } else {
 
-            System.out.println("The number " + num + "is NOT in range between 0 and 1");
+            System.out.println("The number " + num + " is NOT in range between 0 and 1");
         }
     }
 }
