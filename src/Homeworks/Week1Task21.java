072f3dd08dd3b491952efd0fdf67222bd94a2e4a
SalOnGit <1salnik1@gmail.com>
2020-01-21 00:34:12 +0200

    Initial commit

---
+++ src/Homeworks/Week1Task21.java
@@ -0,0 +1,22 @@
+package Homeworks;
+
+public class Week1Task21 {
+
+    public static void main(String[] args) {
+
+        int A,B,C;
+
+        if (A+B+C>0) {
+
+            A*=2;
+            B*=2;
+            C*=2;
+        } else {
+
+            A=0;
+            B=0;
+            C=0;
+        }
+
+    }
+}
---
+++ src/Homeworks/Week1Task22.java
@@ -0,0 +1,11 @@
+package Homeworks;
+
+public class Week1Task22 {
+
+    public static void main(String[] args) {
+
+       int number = 123456;
+
+       
+    }
+}
