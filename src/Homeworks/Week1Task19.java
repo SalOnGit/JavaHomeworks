822ba54359d77de195a0463e49e6298de2682b26
SalOnGit <1salnik1@gmail.com>
2020-01-20 23:16:38 +0200

    Initial commit

---
+++ src/Homeworks/Week1Task19.java
@@ -0,0 +1,29 @@
+package Homeworks;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Week1Task19 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner (System.in);
+
+        System.out.print("Input the number from 1 till 99: ");
+
+        int number = sc.nextInt();
+
+        if (!(number > 1 && number < 99)) {
+
+            System.out.println("Enter the number from 1 to 99 please!");
+        } else
+
+            if ((number == 1) && ((number%10) == 1)) {
+
+                System.out.println(number + "ка");
+            }
+
+            }
+
+    }
+}
