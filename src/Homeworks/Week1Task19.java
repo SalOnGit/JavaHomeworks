ddf86654537f47b56a985ce245612ab11f8ed8d9
SalOnGit <1salnik1@gmail.com>
2020-01-20 23:48:15 +0200

    Initial commit

--- src/Homeworks/Week1Task19.java
+++ src/Homeworks/Week1Task19.java
@@ -16,5 +16,22 @@ public class Week1Task19 {
 
             System.out.println("Enter the number from 1 to 99 please!");
         }
+
+        if (number == 1 || number%10 == 1) {
+
+            System.out.println(number + " kopeyka");
+        }
+
+        if ((number >= 2 && number <= 4) || (number%10 >= 2 && number%10 <= 4)) {
+
+            System.out.println(number + " kopeyki");
+        }
+
+        if ((number >= 5 && number <= 14) || number%10 >= 5 || number%10 == 0) {
+
+            System.out.println(number + " kopeek");
+
+        }
+
     }
 }
